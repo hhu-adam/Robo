dabfb7b16c3626a9e5878abca454aad635559905
Marcus Zibrowius <marcus.zibrowius@cantab.net>
2023-12-08 15:31:40 +0100

    add missing files

---
+++ Game/Levels/Implication/L10_ByCases.lean
@@ -0,0 +1,34 @@
+import Game.Metadata
+
+
+
+World "Implication"
+Level 10
+
+Title "by_cases"
+
+Introduction
+"
+Beim nächsten Problem stutzt der Operationsleiter.
+
+**Operationsleiter**:  Ehrlich gesagt weiß ich gar nicht, wo dieses Blatt herkommt.  Das ist gar nicht von mir.  Sieht aber irgendwie interessant aus.
+"
+
+Statement (A : Prop) : ¬A ∨ A := by
+  Hint "**Du**: Naja, eine der beiden Aussagen `A` oder `¬A` wird schon wahr sein.  
+
+**Robo**: Klarer Fall für eine Fallunterscheidung, würde ich sagen. Probier mal `by_cases h : A`."
+  by_cases h : A
+  Hint "**Robo**: Siehst du, jetzt hat der Beweis zwei Teile.  Im ersten Teil nimmst du an, dass `A` wahr ist. Im zweiten nimmst du an, dass `A` falsch ist."
+  right
+  assumption
+  left
+  assumption
+
+Conclusion
+"
+Der Operationsleiter nickt zustimmend.
+"
+
+NewTactic by_cases
+DisabledTactic tauto
