4cf0a94d1b062c48d7f2cf57348e3eb6528b4354
Jon Eugster <eugster.jon@gmail.com>
2024-02-14 14:04:46 +0100

    add a user solution

--- Game/Levels/SetTheory/L05_Empty.lean
+++ Game/Levels/SetTheory/L05_Empty.lean
@@ -20,6 +20,32 @@ open Set
 Statement Set.eq_empty_iff_forall_not_mem {A : Type _} (s : Set A) :
     s = ∅ ↔ ∀ x, x ∉ s := by
   Hint "Das Lemma `subset_empty_iff` von letzter Aufgabe könnte hilfreich sein."
+  Branch
+    -- User solution by Yagub A.
+    constructor
+    · intro h
+      rw[h]
+      tauto
+    · intro h
+      by_contra h0
+      rw[Subset.antisymm_iff] at h0
+      rw[not_and_or] at h0
+      rcases h0 with h8 | h8
+      · rw[subset_def] at h8
+        rw[not_forall] at h8
+        Hint (hidden := true) "jetzt könntest du {h8} mit `rcases` aufteilen"
+        rcases h8 with ⟨x, hx⟩
+        apply hx
+        intro hxs
+        have hxs2 := h x
+        contradiction
+      · rw[subset_def] at h8
+        rw[not_forall] at h8
+        rcases h8 with ⟨x, hx⟩
+        apply hx
+        intro hxs
+        have hxs2 := h x
+        contradiction
   rw [←subset_empty_iff]
   rfl -- This is quite a miracle :)
 
