3f7591f7c8243df54a80514476dee354cdbb884d
Jon Eugster <eugster.jon@gmail.com>
2024-03-21 15:42:50 +0100

    fix lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -11,8 +11,7 @@ def LocalGameServer : Dependency := {
 
 def RemoteGameServer : Dependency := {
   name := `GameServer
-  src := Source.git "https://github.com/leanprover-community/lean4game.git" "v4.6.0" "server"
-  -- TODO: change back to `leanVersion`
+  src := Source.git "https://github.com/leanprover-community/lean4game.git" leanVersion "server"
 }
 
 /- Choose GameServer dependency depending on the environment variable `LEAN4GAME`. -/
