0b632ffddbf3d2ec6d0c5e85c114bde742bcb302
Marcus Zibrowius <marcus.zibrowius@cantab.net>
2024-04-17 11:59:11 +0200

    modified:   L02_EBasis.lean

--- Game/Levels/MatrixTrace/L02_EBasis.lean
+++ Game/Levels/MatrixTrace/L02_EBasis.lean
@@ -9,16 +9,15 @@ Title "Falsche Indizes"
 
 Introduction
 "
-Kurze Zeit später findet ihr noch mals einen sehr ähnlichen Zettel, wieder
-vollgekritzelt und vieles sieht falsch oder durchgestrichen aus.
+Kurze Zeit später findet ihr einen sehr ähnlichen Zettel, wieder
+vollgekritzelt, fast alles durchgestrichen.
 Die erste Zeile aber lässt sich gerade noch ausmachen.
 "
 
 Conclusion "
-  **Robo**: Ich speichere dieses `E.mul_of_ne` auch mal, wer weiss.
+  **Robo**: Ich speichere dieses `E.mul_of_ne` auch mal, wer weiß.
 
-  **Du**: Jetzt bin ich aber neugierig, wer das ist. Komm, lass uns
-  weitergehen.
+  **Du**: Jetzt bin ich aber neugierig, wer diese Zettel hier verloren oder weggeworfen hat. Komm, lass uns weitergehen.
 "
 
 open Nat Matrix BigOperators
@@ -29,7 +28,7 @@ TheoremDoc Matrix.E.mul_of_ne as "E.mul_of_ne" in "Matrix"
 -- @[inherit_doc Matrix.StdBasisMatrix.mul_of_ne]
 Statement Matrix.E.mul_of_ne {n : ℕ} (i j : Fin n) {k l : Fin n} (h : j ≠ k) : E i j * E k l = 0 := by
   Hint "**Du**: Vielleicht sind das wirklich irgendwelche Tipps. Der hier scheint
-  ganz ähnlich zu funktionieren: Wenn man zwei Standardmatrizen zusammenzählt und die Indizes nicht stimmen, kriegt man Null.
+  ganz ähnlich zu funktionieren: Wenn man zwei Standardmatrizen zusammenzählt und die Indizes nicht stimmen, erhält man Null.
   "
   unfold E
   Hint (hidden := true) "**Robo**: Vergiss aber nicht, dass `simp` die Annahme `{h}` explizit braucht!"
