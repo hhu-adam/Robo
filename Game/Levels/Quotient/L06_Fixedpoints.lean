65af14c63f126410e5cf4529fe28422d634adeb7
sinhp <sinahazratpour@gmail.com>
2024-04-22 14:15:26 +0200

    Update L06_Fixedpoints.lean
    -- JE: Another Level, leading up to this one. Could even go into the "FixedPoints"-planet
    -- we talked about
    -- SH: I have put it in the Cantor/fixedpoint planet, we use `range_fixedPoints` in the proof which is proved there.

--- Game/Levels/Quotient/L06_Fixedpoints.lean
+++ Game/Levels/Quotient/L06_Fixedpoints.lean
@@ -3,6 +3,7 @@ import Game.Metadata.StructInstWithHoles
 
 import Game.Levels.Quotient.L04_Respect
 import Game.Levels.Quotient.L05_Bijection
+import Game.Levels.Cantor.L06_fixedPoints_range
 
 World "Quotient"
 Level 6
@@ -16,28 +17,9 @@ In this level you show that for an idempotent function `f : A -> A`, the types `
 
 open Set Function Setoid Cardinal
 
-#check range
-
--- JE: Another Level, leading up to this one. Could even go into the "FixedPoints"-planet
--- we talked about
-theorem tmp₀ (f : Function.End A) (h : f ∘ f = f) : range f = fixedPoints f := by
-  apply Subset.antisymm
-  · intro x hx
-    rcases hx
-    rw [← h_1]
-    unfold fixedPoints
-    unfold IsFixedPt
-    rw [mem_setOf]
-    apply congr_fun at h -- :D
-    simp only [comp_apply] at h
-    rw [h]
-  · intro x hx
-    rw [mem_range] -- is simp
-    use x
-    trivial
 
 Statement (f : Function.End A) (h : f ∘ f = f) : Quotient (ker f) ≃ fixedPoints f := by
   trans -- apply Equiv.trans
   exact Equiv.ofBijective (Quotient.lift (rangeFactorization f) respects_ker_rel) (bij_quotient_lift_range_fac f)
-  rw [tmp₀]
+  rw [range_fixedPoints]
   assumption
