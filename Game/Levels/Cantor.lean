4299e636392dd5ebc1b5ca97369194e5476b9cdd
Jon Eugster <eugster.jon@gmail.com>
2024-04-24 18:10:16 +0200

    add missing file

---
+++ Game/Levels/Cantor.lean
@@ -0,0 +1,16 @@
+import Game.Levels.Cantor.L01_IsFixedPt_abs
+import Game.Levels.Cantor.L02_IsFixedPt_not
+import Game.Levels.Cantor.L03_fixedPoints_neg
+import Game.Levels.Cantor.L04_fixedPoints_odd
+import Game.Levels.Cantor.L05_image_fixedPoints
+import Game.Levels.Cantor.L06_fixedPoints_range
+import Game.Levels.Cantor.L07_fixedPoints_Cantor
+import Game.Levels.Cantor.L08_Cantor_Power
+
+World "Cantor"
+Title "Cantor"
+
+Image "images/WIP.png"
+
+Introduction "
+"
