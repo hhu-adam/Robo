b21ea3c1e0069020ba13074eff7456aabe78b448
Jon Eugster <eugster.jon@gmail.com>
2024-04-26 01:01:59 +0200

    add Sina's proof

--- Game/Levels/Cantor/L09_SequenceUncountable.lean
+++ Game/Levels/Cantor/L09_SequenceUncountable.lean
@@ -1,4 +1,5 @@
 import Game.Metadata
+import Game.Levels.Cantor.L08_CantorPowerset
 
 World "Cantor"
 Level 9
@@ -10,8 +11,16 @@ Introduction
 In this level you show that no sequence can exhaust `ℕ → ℕ`.
 "
 
-open Set Function
+open Nat Set Function
 
-Statement (f : ℕ → ℕ → ℕ) : ∃ (g : ℕ → ℕ), ∀ (n : ℕ), g ≠ f n := by
-
-  sorry
+Statement (f : ℕ → ℕ → ℕ) : ∃ (g : ℕ → ℕ), ∀ (n : ℕ), f n ≠ g := by
+  have h : ¬ Surjective f := by
+    intro h
+    apply cantor_diagonal at h
+    specialize h succ
+    obtain ⟨n, hn⟩ := h
+    dsimp [IsFixedPt] at hn
+    simp only [Nat.succ_ne_self] at hn
+  unfold Surjective at h
+  push_neg at h
+  assumption
