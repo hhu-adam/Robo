1eaf004865c175e5217fa5e77cb7d19a1e2d6c1e
Jon Eugster <eugster.jon@gmail.com>
2024-04-29 17:10:37 +0200

    add mk_all script

---
+++ mk_all.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+cd "$(git rev-parse --show-toplevel)" || exit 1
+
+for planet in Game/Levels/*/; do
+  FILE="$(dirname $planet)/$(basename $planet).lean"
+  sed -i '/^import /d' "$FILE"
+
+  #sed -i '1i import Bugs\nimport More' "$(dirname $planet)/$(basename $planet).lean"
+
+  NEW=`git ls-files "$planet/*.lean" | LC_ALL=C sort | sed 's/\.lean//;s,/,.,g;s/^/import /'`
+
+  echo "\n\n\n"
+  echo $NEW
+
+  echo -e "$NEW\n$(cat $FILE)" > $FILE
+done
+
+#git ls-files "Game/Levels/*/" | LC_ALL=C sort | sed 's/\.lean//;s,/,.,g;s/^/import /'  > "Game.lean"
+
+
+# for gp in Mathlib MathlibExtras Mathlib/Tactic Counterexamples Archive
+# do
+#   git ls-files "$gp/*.lean" | LC_ALL=C sort | sed 's/\.lean//;s,/,.,g;s/^/import /' > "$gp.lean"
+# done
