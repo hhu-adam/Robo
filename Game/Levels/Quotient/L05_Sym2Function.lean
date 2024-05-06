624b2465cc9552f509ebb2085c939b9bbbee4271
sinhp <sinahazratpour@gmail.com>
2024-05-06 10:14:25 +0200

    replacing Quotient.mk'
    replacing `Quotient.mk'` by `Quotient.mk s` and inserting `let s := Sym2.Rel.setoid A` in the hypotheses

--- Game/Levels/Quotient/L05_Sym2Function.lean
+++ Game/Levels/Quotient/L05_Sym2Function.lean
@@ -23,10 +23,9 @@ typeclass inference. Other than this it works the same way as `Quotient.mk`.
 
 open List Sym
 
-attribute [local instance] Sym2.Rel.setoid
-
 Statement (f : Sym2 A → B) :
-    ∀ a₁ a₂, (f ∘ Quotient.mk') (a₁ , a₂) = (f ∘ Quotient.mk') (a₂ , a₁) := by
+    let s := Sym2.Rel.setoid A
+    ∀ a₁ a₂, (f ∘ Quotient.mk s) (a₁ , a₂) = (f ∘ Quotient.mk s) (a₂ , a₁) := by
   intro a₁ a₂
   dsimp
   Branch
@@ -34,15 +33,3 @@ Statement (f : Sym2 A → B) :
   congr 1
   apply Quotient.sound
   apply Sym2.Rel.swap
-
-
--- Statement (f : Sym2 A → B) :
---     let g : A × A → B := fun (a₁ , a₂) ↦  f ⟦ (a₁, a₂) ⟧
---     ∀ a₁ a₂, g (a₁ , a₂) = g (a₂ , a₁) := by
---   intro a₁ a₂
---   simp [g]
---   Branch
---     apply congr_arg f
---   congr 1
---   apply Quotient.sound
---   apply Sym2.Rel.swap
