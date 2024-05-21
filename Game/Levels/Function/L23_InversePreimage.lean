e1d3b04436501d42a42a1ed9b76c9f0faeceaa13
sinhp <sinahazratpour@gmail.com>
2024-05-21 11:21:28 +0200

    improving the proof

--- Game/Levels/Function/L23_InversePreimage.lean
+++ Game/Levels/Function/L23_InversePreimage.lean
@@ -17,7 +17,7 @@ f '' S = {b : B | ∃ a : A, a ∈ S ∧ f a = b}
 ```
 
 Note that an element of the image is a triple `⟨b, a, h⟩` where `b` is the image of `a` and `h`
-is the proof that `a` is in `S`.
+is the proof that `a` is in `S` and `f a = b`.
 
 The image of function with a left inverse is a subset of the preimage of the inverse of
 the same subset.
@@ -28,7 +28,10 @@ open Function Set
 Statement image_subset_preimage_of_inverse {f : A → B} {g : B → A} (hL : LeftInverse g f)
     (S : Set A) :
     f '' S ⊆ g ⁻¹' S := by
-  rintro b h₁
-  obtain ⟨x, hx, e⟩ := h₁
+  intro b hb
+  obtain ⟨x, hx, e⟩ := hb
+  dsimp [LeftInverse] at hL
   rw [← hL x, e] at hx
-  exact hx
+  Branch
+    apply hx
+  assumption
