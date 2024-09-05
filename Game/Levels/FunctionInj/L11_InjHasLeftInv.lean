6adb00e9ebf1a4fc8ea048b6e4a7665a7ee01af0
Marcus Zibrowius <marcus.zibrowius@cantab.net>
2024-09-05 10:21:00 +0200

    Inj L11 as an iff statement, without range

--- Game/Levels/FunctionInj/L11_InjHasLeftInv.lean
+++ Game/Levels/FunctionInj/L11_InjHasLeftInv.lean
@@ -4,44 +4,63 @@ import Game.Metadata
 World "FunctionInj"
 Level 11
 
-Title "Injections with nonempty domain have retract."
+Title "Injections have a left inverse, and vice versa"
 
 Introduction
 "
-  In this level you show that an injective function with a nonempty domain has a left inverse.
 "
 
 open Function Set
 
--- attribute [local instance] Classical.propDecidable
 
-Statement Injective.hasLeftInverse {A B : Type} [Nonempty A] (f : A → B) (hf : Injective f) :
-    HasLeftInverse f := by
-  -- Branch
-  --   let finv : B → A := fun b => if h : ∃ x, f x = b then h.choose else Classical.arbitrary A
-  --   use finv
-  --   unfold LeftInverse
-  --   intro x
-  --   apply hf
-  --   simp only [finv, dif_pos (⟨x, rfl⟩ : ∃ x', f x' = f x)]
-  --   apply Exists.choose_spec (⟨x, rfl⟩ : ∃ x', f x' = f x)
+-- TODO: new level with this!
+-- Needed as a have statement in the Boss level
+Statement {A B : Type} [hA : Nonempty A] (f : A → B ) : ∀ b : B, ∃ a : A, f a = b ∨ ¬ ∃ a' : A , f a' = b   := by
   have a₀ : A := Classical.arbitrary A
-  -- TODO: new level with this!
-  have : ∀ b : B, ∃ a : A, b ∈ range f → f a = b := by
-    intro b
-    by_cases hb : b ∈ range f
-    obtain ⟨a,ha⟩ := hb
+  intro b
+  by_cases hb : ∃ a' : A, f a' = b 
+  · obtain ⟨a,ha⟩ := hb
     use a
-    intro _h
+    left
     assumption
-    use a₀
-    intro h
-    contradiction
-  choose g hg using this
-  use g
-  intro a
-  apply hf
-  apply hg
-  simp
+  use a₀
+  right
+  assumption
 
-NewTheorem dif_pos
+
+-- mathlib: Function.injective_iff_hasLeftInverse
+Statement injective_iff_hasLeftInverse {A B : Type} [hA : Nonempty A]  (f : A → B) :
+  Injective f ↔ HasLeftInverse f := by
+  constructor
+  · intro hf
+    have : ∀ b : B, ∃ a : A, f a = b ∨ ¬ ∃ a' : A , f a' = b := by
+      /- exactly a previous level, now without hints -/
+      have a₀ : A := Classical.arbitrary A
+      intro b
+      by_cases hb : ∃ a' : A, f a' = b 
+      · obtain ⟨a,ha⟩ := hb
+        use a
+        left
+        assumption
+      use a₀
+      right
+      assumption
+    choose g hg using this
+    use g
+    intro a
+    apply hf
+    obtain hpos | hneg := hg (f a)
+    · assumption
+    · push_neg at hneg
+      have : f a ≠ f a := hneg a 
+      contradiction
+  · /- Injective f → HasLeftInverse f 
+       exactly a previous level, now without hints-/
+    intro hL
+    intro a a' ha
+    obtain ⟨g, hg⟩ := hL
+    apply congr_arg g at ha
+    unfold LeftInverse at hg
+    rw [hg a, hg a'] at ha
+    assumption
+
