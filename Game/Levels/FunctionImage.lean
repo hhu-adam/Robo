c309b7c56a812ddce832714c1988f04928dc86d1
Jon Eugster <eugster.jon@gmail.com>
2024-09-05 12:54:49 +0200

    update FunctionImage

--- Game/Levels/FunctionImage.lean
+++ Game/Levels/FunctionImage.lean
@@ -1,17 +1,11 @@
 import Game.Levels.FunctionImage.L01_ImagePreimage
 import Game.Levels.FunctionImage.L02_Image
-import Game.Levels.FunctionImage.L02_ImagePreimage
-import Game.Levels.FunctionImage.L03_InjectiveRange
 import Game.Levels.FunctionImage.L03_SurjectiveRange
 import Game.Levels.FunctionImage.L04_ImagePreimage
-import Game.Levels.FunctionImage.L04_LeftInvPreimage
 import Game.Levels.FunctionImage.L05_InjectiveFibre
-import Game.Levels.FunctionImage.L05_Preimage_Injective
 import Game.Levels.FunctionImage.L06
-import Game.Levels.FunctionImage.L06_SurjectiveRange
 import Game.Levels.FunctionImage.L07_LeftInvPreimage
 import Game.Levels.FunctionImage.L08_Preimage_Injective
-import Game.Levels.FunctionImage.LXX_Set6
 
 World "FunctionImage"
 Title "Image/Preimage"
