66c2bfad0247ed587379d8052c4beac3ef21e30f
Jon Eugster <eugster.jon@gmail.com>
2024-09-06 13:03:38 +0200

    use macro "constructor := fconstructor"

--- Game/Metadata/Tactic.lean
+++ Game/Metadata/Tactic.lean
@@ -1,4 +1,5 @@
 import Game.Metadata.Tactic.ByCases
+import Game.Metadata.Tactic.Constructor
 import Game.Metadata.Tactic.Induction
-import Game.Metadata.Tactic.Ring
 import Game.Metadata.Tactic.LinearCombination
+import Game.Metadata.Tactic.Ring
---
+++ Game/Metadata/Tactic/Constructor.lean
@@ -0,0 +1,11 @@
+import Mathlib.Tactic.Ring
+
+namespace Game
+
+@[inherit_doc Lean.Parser.Tactic.constructor]
+macro (name := constructor) "constructor" : tactic =>
+  `(tactic| fconstructor)
+
+extend_docs Game.constructor after "The variant used in the Robo game is called
+`fconstructor` in Mathlib. The only difference is the real `constructor` sorts
+the goals in a way not ideal for the game."
