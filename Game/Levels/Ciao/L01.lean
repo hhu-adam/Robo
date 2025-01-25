677b519d69a0a74886fbdf69cc3e31054f3b881a
Marcus <marcus.zibrowius@cantab.net>
2025-01-25 13:52:59 +0100

    add warning that server connection will get lost to Ciao

--- Game/Levels/Ciao/L01.lean
+++ Game/Levels/Ciao/L01.lean
@@ -12,23 +12,37 @@ Statement : ∀ (n : ℕ), ∃ (m : ℕ), m > n := by
   use n+1
   linarith
 
-Conclusion "**Du**: Das war ja nun nicht so schwer …
+Conclusion "**Du**: Das war ja nun nicht so schwer …  Wer die wohl gesendet hat?  Und warum?
 
-**Robo**:  Ich glaube, das war Leansch für „Es geht immer weiter …”.
+**Robo**:  Das war sicher eine Nachricht von Ritha …
+
+Robo schaut in die Ferne.
+
+**Robo**:  Ich glaube, sie wollte uns ermuntern, weiter zu fliegen.
+
+**Du**: Aber wohin?
 
 **Robo**:  Oh, schau mal, noch eine Nachricht:
 
+*Liebe Erdwesen,*
 
-*Liebe Erdwesen, es war uns ein Vergnügen, euch kennenzulernen!
+*es war uns ein Vergnügen, euch kennenzulernen!
 Es tut uns aufrichtig leid, dass ihr euch in unser Formaloversum verirrt habt und
 nicht wieder nach Hause findet.  Aber wir haben noch eine gute Nachricht:
 Ihr seid nicht die einzigen!
 Fliegt nur rasch weiter zum Planeten Zulip.
 Dort werdet ihr viele weitere Erdwesen finden, die sich ins Formaloversum verirrt haben.
-Zulip ist groß, gewiss werdet auch ihr dort einen neues Zuhause finden.  Hier die Koordinaten:*
+Zulip ist groß, gewiss werdet auch ihr dort ein neues Zuhause finden.  Hier die Koordinaten:*
 
 [248-4804-180 | 844-1001-553](https://leanprover.zulipchat.com/#narrow/channel/113489-new-members/)
 
-Mit den aufrichtigsten Grüßen
-– Eure Formalosophen
+*Mit den aufrichtigsten Grüßen*
+
+*– Eure Formalosophen*
+
+**Robo**:  Na dann – los!
+
+[Nicht wundern:  Wenn ihr den Koordinaten folgt, beschleunigt das Raumschiff auf
+Überlichtgeschwindigkeit und ihr verliert vorübergehend den Kontakt zum Server.
+Das macht aber nichts.  Ihr werdet sicher auf Zulip ankommen.]
 "
