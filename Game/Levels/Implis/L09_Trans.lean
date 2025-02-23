c9112152658a8e04702b813191b2e0a93fe04dd8
TauTastic <91988666+tautastic@users.noreply.github.com>
2025-02-23 12:50:15 +0100

    Fix missing closing backtick

--- Game/Levels/Implis/L09_Trans.lean
+++ Game/Levels/Implis/L09_Trans.lean
@@ -9,7 +9,7 @@ Introduction
 "
 **Du**: Irgendwie fühlen sich diese `rw` an, als würde man von hinten durch den Bauch argumentieren.  Geht das nicht auch irgendwie geradeaus, oder denken alle hier um die Ecke?
 
-**Robo**:  Vielleicht würde dir `trans' besser gefallen.  Damit könntest du deine Kette von Äquivalenzen  $B \\iff A \\iff D \\iff C$ Schritt für Schritt abarbeiten: als erstes führst Du mit `trans A` den Zwischenschritt `B \\iff A` ein, dann mit `trans D` den nächsten Zwischenschritt.
+**Robo**:  Vielleicht würde dir `trans` besser gefallen.  Damit könntest du deine Kette von Äquivalenzen  $B \\iff A \\iff D \\iff C$ Schritt für Schritt abarbeiten: als erstes führst Du mit `trans A` den Zwischenschritt `B \\iff A` ein, dann mit `trans D` den nächsten Zwischenschritt.
 "
 
 Statement (A B C D : Prop) (h₁ : C ↔ D) (h₂ : A ↔ B) (h₃ : A ↔ D) : B ↔ C := by
