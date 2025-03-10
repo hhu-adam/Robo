ae4fcdff32ac6b46aee8e82f612e1ebbf1730c66
TentativeConvert <marcus.zibrowius@cantab.net>
2025-03-10 10:27:01 +0100

    Create overview.lean

---
+++ Game/Levels/Piazza/overview.lean
@@ -0,0 +1,24 @@
+/- Need to introduce/practice
+
+     Finset.Icc_subset_Icc_iff  (needed for BABYLON exercise introucing sub_subset)
+
+
+   Should repeat exercise 
+
+      (m n : ℕ) (h₁ : l ≤ m) (h₂ : m ≤ n) : l ≤ x ∧ x ≤ n → ¬ (m ≤ x ∧ x ≤ n) → x ≤ m 
+
+    from LUNA in Icc-notation.
+
+-/ 
+
+/- Piazza L01 -/
+/- Piazza L02 -/
+/- Piazza L03 -/
+/- Piazza L04 -/
+/- Piazza L05 -/
+/- Piazza L06 -/
+/- Piazza L07 -/
+/- Piazza L08 -/
+/- Piazza L09 -/
+/- Piazza L10 -/
+/- Piazza L11 -/
