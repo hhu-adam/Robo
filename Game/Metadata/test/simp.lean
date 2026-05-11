01d867c74151e5457d63d8162fc19495ed30aa20
TentativeConvert <marcus.zibrowius@cantab.net>
2026-05-11 10:21:47 +0200

    update/simplify custom simp test

--- Game/Metadata/test/simp.lean
+++ Game/Metadata/test/simp.lean
@@ -1,38 +1,16 @@
 import Game.Metadata.Tactic.Simp
+import Mathlib.Algebra.MvPolynomial.CommRing
 
--- behaviour without custom simp (comment import line above)
-/-
-example : 1 + 1 = 2 := by simp                   -- works, as expected
-example : 1 + 1 = 2 := by simp?                  -- suggests simp only [Nat.reducedAdd], as expected
-example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp -- work, as expected
-example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp only [game_simp] fails, as expected
--/
+-- success with standard simp:
+#guard_msgs in
+example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp (config := {})
 
-@[game_simp] theorem my_test_lemma : 1 + 1 = 2 := by norm_num
---#check @game_simp
+-- failure with custom simp before registring any `game_simp` lemmas:
+/-- error -/
+#guard_msgs (error, substring := true) in
+example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp
 
-example : 1 + 1 = 2 := by simp                   -- works (as expected)
-example : 1 + 1 = 2 := by simp?                  -- suggests simp only [my_test_lemma], as intended
---example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp  -- fails, as intended
---example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp? -- fails, as intended
+-- success with costum simp after registering relevant `game_simp` lemmas:
 attribute [game_simp] Finset.sum_const smul_eq_mul mul_one
-example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp  -- now works, as intended
-example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp? -- now works, as intended
-
-
-/-macro "true_simp" : tactic
-  => `(tactic| simp (config := {}))
-macro "true_simp" "[" args:Lean.Parser.Tactic.simpLemma,* "]" : tactic
-  => `(tactic| simp (config := {}) [$args,*])
-macro "true_simp" "at" loc:Lean.Parser.Tactic.locationHyp : tactic
-  => `(tactic| simp (config := {}) at $loc)
-macro "true_simp" "[" args:Lean.Parser.Tactic.simpLemma,* "]" "at" loc:Lean.Parser.Tactic.locationHyp : tactic
-  => `(tactic| simp (config := {}) [$args,*] at $loc)
--/
-
---example : 1 + 1 = 2 := by true_simp                   -- works (as expected)
-example : 1 + 1 = 2 := by true_simp?                  -- suggests [Nat.reducedAdd]
---example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by true_simp  -- works, as intended
-example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by true_simp? -- works, as intended
-
-#print Lean.Meta.SimpTheorems
+#guard_msgs in
+example (I : Finset ℕ) : (∑ i ∈ I, 1) = Finset.card I := by simp
