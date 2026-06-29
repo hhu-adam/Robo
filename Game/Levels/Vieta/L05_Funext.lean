f9f2164432d75532b985e479c5df2631c0d630b6
Wenrong Zou <141128015+WenrongZou@users.noreply.github.com>
2026-06-29 10:25:09 +0100

    modify Vieta/Level05 (#135)
    modify the problem intro `funext` in Vieta L05 so grind cannot solve it before funext

--- Game/Levels/Vieta/L05_Funext.lean
+++ Game/Levels/Vieta/L05_Funext.lean
@@ -17,21 +17,23 @@ Introduction "Intro Vieta L05"
 
 open Function
 
-Statement :
-    let f := fun (x : ℤ) ↦ x ^ 2;
+Statement {n : ℕ} :
+    let f := fun (x : ℤ) ↦ x ^ (2 * n);
     let g := fun x ↦ f (-x);
     f = g := by
-    /-
+/-
   Hint"**Du**: Per Definition sind doch zwei Abbildungen gleich, wenn sie angewendet auf
-jedes Element den gleichen Wert haben …
+  jedes Element den gleichen Wert haben …
 
-**Robo**: Zu dem Prinzip hätte ich die Taktik `funext` auf Lager.
-Mit `funext x` wählst du ein beliebiges `x` und änderst das Beweisziel von `f = g` zu `f x = g x`."
+  **Robo**: Zu dem Prinzip hätte ich die Taktik `funext` auf Lager.
+  Mit `funext x` wählst du ein beliebiges `x` und änderst das Beweisziel von `f = g` zu `f x = g x`."
 -/
   Hint "Try `funext`. Explain `funext x` as taking arbitrary `x` and changing goal from `f = g` to `f x = g x`"
   funext x
-  -- Hint (hidden := true) "**Robo**: Zur Erinnerung, `ring` sieht durch lokale Definition hindurch."
-  Hint (hidden := true) "Remind: `ring` sees through local definitions"
+  unfold g f
+  rw [Even.neg_pow]
+  use n
+  Hint (hidden := true) "Reminder: `ring` and `grind` can see through local definitions"
   ring
 
 OnlyTactic funext ring
