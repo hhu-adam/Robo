747ac2c28e18be981bdeb5dd721a9a433806deaa
WenrongZou <wenrongzou@outlook.com>
2026-07-11 22:18:55 +0200

    activate RealUncountable

---
+++ Game/Levels/Uncountable/RealUncountable_01.lean
@@ -0,0 +1,59 @@
+import Game.Metadata
+import Mathlib.SetTheory.Cardinal.Arithmetic
+
+World "TmpRealsUncountable"
+Level 1
+
+Title "überabzählbare reele Zahlen"
+
+Introduction "Zuerst zeigen wir ein Hilfslemma."
+
+universe u
+
+open Module
+
+-- Damit man `#K` anstatt `Cardinal.mk K` für die Kardinalität von `K` schreiben kann
+namespace Cardinal
+
+/- Sei $K$ ein Körper mit Kardinalität $\aleph_0$ und sei $V$ ein $K$-Vektorraum.
+
+Zeige, dass die Kardinalität von $V \le \aleph_0$. -/
+
+/---/
+TheoremDoc Cardinal.cardinal_eq_of_finite_basis as "Cardinal.cardinal_eq_of_finite_basis" in "Cardinal"
+
+Statement cardinal_eq_of_finite_basis {K V : Type u} {ι : Type u} [Field K] [AddCommGroup V]
+    [Module K V] [Fintype ι] (h_card : #K = ℵ₀) (h_basis : Basis ι K V) : #V ≤ ℵ₀:= by
+  Hint "Als Beweisstrategie möchtest du wie `#K ^ #ι ≤ ℵ₀` gehen. Also zuerst sagen,
+  dass die Kardinalität von $V$ genau die Kardinalität von $K$ hoch $\\mathrm\{dim}(V)$ ist."
+  Hint "Schau mal `have h := {h_basis}.equivFun.toEquiv` an"
+  rw [Cardinal.mk_congr (h_basis.equivFun.toEquiv)]
+  rw [← Cardinal.power_def]
+  rw [h_card]
+  rw [Cardinal.mk_fintype, ]
+  rw [power_natCast]
+  apply Cardinal.power_nat_le
+  rfl
+
+  -- simp only [Cardinal.mk_fintype, Cardinal.pow_cast_right]
+  -- apply Cardinal.power_nat_le
+  -- rfl
+  -- TODO: Project by Eduart Bopp
+
+/---/
+TheoremDoc Cardinal.mk_congr as "Cardinal.mk_congr" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.power_def as "Cardinal.power_def" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.mk_fintype as "Cardinal.mk_fintype" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.power_nat_le as "Cardinal.power_nat_le" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.power_natCast as "Cardinal.power_natCast" in "Cardinal"
+
+NewTheorem Cardinal.mk_congr Cardinal.power_def Cardinal.mk_fintype
+  Cardinal.power_nat_le Cardinal.power_natCast
---
+++ Game/Levels/Uncountable/RealUncountable_02.lean
@@ -0,0 +1,98 @@
+import Game.Metadata
+import Game.Levels.Uncountable.RealUncountable_01
+import Mathlib.SetTheory.Cardinal.Arithmetic
+import Mathlib.LinearAlgebra.FiniteDimensional.Defs
+import Mathlib.Data.Rat.Denumerable
+import Mathlib.Analysis.Real.Cardinality
+
+World "TmpRealsUncountable"
+Level 2
+
+Title "überabzählbare reele Zahlen"
+
+Introduction
+"
+Wir zeigen dass ℝ ein nicht-endlicher ℚ-Vektorraum ist.
+"
+
+
+open Module
+
+-- Damit man `#K` anstatt `Cardinal.mk K` für die Kardinalität von `K` schreiben kann
+namespace Cardinal
+
+/-
+Zeige dass ℝ kein endlich dimensionaler ℚ-Vektorraum ist.
+-/
+
+Statement : ¬ FiniteDimensional ℚ ℝ := by
+  Branch
+    -- TODO: Eduart Bopp's Beweis
+    Hint "
+    `let B := Basis.ofVectorSpace ℚ ℝ` gibt dir eine Basis von $\\mathbb\{R}$ über $\\mathbb\{Q}$.
+    Mache dann einen Widerspruchsbeweis und brauche folgende Resultate:
+
+    * `Cardinal.not_countable_real`: Das Hilfslemma von vorhin.
+    * `Cardinal.not_countable_real` : $\\mathbb\{R}$ ist überabzählbar.
+
+    Fange vielleicht auch damit an, `#ℚ = ℵ₀` zu zeigen.
+    "
+    let B := Basis.ofVectorSpace ℚ ℝ
+    -- beginnt Gegenbeweis
+    by_contra
+    -- Angenomme R wäre ein endlich dimensionaler ℚ-Vektorraum ist.
+
+    -- Mein Teil
+
+    -- Schreibe die Kardinalität von `ℚ` als ℵ₀
+    letI : Countable ℚ := inferInstance
+    have h_ℚ : #ℚ = Cardinal.aleph0 := Cardinal.mk_eq_aleph0 ℚ
+
+    -- Anwenden des Lemmas auf die Basis `B` von `ℝ` über `ℚ`
+    --#R <= #Q
+    have cardinal_ineq : #ℝ ≤ Cardinal.aleph0 :=
+      cardinal_eq_of_finite_basis h_ℚ B
+
+    -- R überabzählbar
+    have h3 := Cardinal.not_countable_real
+
+    -- ¬#↥set.univ ≤ ℵ₀, set.univ = R
+    rw [← Cardinal.le_aleph0_iff_set_countable] at h3
+    -- schreibe um zu ¬#ℝ ≤ ℵ₀
+    simp only [Cardinal.mk_univ] at h3
+
+    -- Q abzählbar, widerspruch
+    contradiction
+
+  by_contra h
+  -- Widerspruch: Wir wissen dass ℝ nicht countable ist.
+  apply Cardinal.not_countable_real
+  rw [← Cardinal.le_aleph0_iff_set_countable]
+  rw [Cardinal.mk_univ]
+  let B := Basis.ofVectorSpace ℚ ℝ
+  apply cardinal_eq_of_finite_basis _ B
+  simp only [Cardinal.mk_denumerable]
+
+/---/
+TheoremDoc Cardinal.not_countable_real as "Cardinal.not_countable_real" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.le_aleph0_iff_set_countable as "Cardinal.le_aleph0_iff_set_countable" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.mk_univ as "Cardinal.mk_univ" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.mk_denumerable as "Cardinal.mk_denumerable" in "Cardinal"
+
+/---/
+TheoremDoc Cardinal.mk_le_aleph0_iff as "Cardinal.mk_le_aleph0_iff" in "Cardinal"
+
+/---/
+DefinitionDoc Basis.ofVectorSpace as "Basis.ofVectorSpace" in "LinearAlgebra"
+
+NewTheorem Cardinal.not_countable_real Cardinal.le_aleph0_iff_set_countable
+  Cardinal.mk_univ Cardinal.cardinal_eq_of_finite_basis Cardinal.mk_denumerable
+  Cardinal.mk_le_aleph0_iff
+
+NewDefinition Basis.ofVectorSpace
